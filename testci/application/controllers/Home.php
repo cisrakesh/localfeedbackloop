d6f14c67c1403672f70c54bc110a5a535e9a5d95
cis <cis@CISM188.(none)>
2016-04-28 14:52:33 +0530

    final commit

--- testci/application/controllers/Home.php
+++ testci/application/controllers/Home.php
@@ -10,24 +10,44 @@ class Home extends CI_Controller {
 	public function index()
 	{
 		
-		$json_url = "http://test.localfeedbackloop.com/api?apiKey=61067f81f8cf7e4a1f673cd230216112&noOfReviews=10&internal=0&yelp=1&google=1&offset=50&threshold=0";
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
-		curl_setopt($ch, CURLOPT_URL, $json_url);
-		$result = json_decode(curl_exec($ch));
-		var_dump ($result);
-
-		$curl_errno = curl_errno($ch);
-		$curl_error = curl_error($ch);
-
-		curl_close($ch);
+		$result=$this->getfeedback(1);
+		$totalreview=$result['business_info']['total_rating']['total_no_of_reviews'];
+		$pages=intval($totalreview/10);
+		$extrarecord=$totalreview%10;
+		if($extrarecord>0){
+			$pages++;
+		}
+		$result['pages']=$pages;
+		
 
 		// ON SUCCESS
 
-		//$json_a =  $result[10];
+		//$json_a =  $result[10];*/
+		//$result = file_get_contents($json_url);
+		// Will dump a beauty json :3
+		//print_r(json_decode($result, true));
 		
 		
-		$this->load->view('home');
+		$this->load->view('home',$result);
+	}
+
+	public function paginate(){
+		
+		$result=$this->getfeedback($this->input->post('pageNumber'));
+		echo json_encode($result['reviews']);
+	}
+
+	function getfeedback($pageNumber){
+		$offset=($pageNumber-1)*10;
+		$json_url = "http://test.localfeedbackloop.com/api?apiKey=61067f81f8cf7e4a1f673cd230216112&noOfReviews=10&internal=0&yelp=1&google=1&offset=".$offset."&threshold=0";
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+		curl_setopt($ch, CURLOPT_URL, $json_url);
+		//htmlspecialchars_decode(string)
+		$curlResult=curl_exec($ch);
+		$result = json_decode($curlResult,true);
+		curl_close($ch);
+		return $result;
 	}
 }
--- testci/application/views/home.php
+++ testci/application/views/home.php
@@ -79,14 +79,9 @@
                                 <div class="tab-pane active" id="search-tab-projects">
                                     <!-- Search Info - Pagination -->
                                     <div class="block-section clearfix">
-                                        <ul class="pagination remove-margin pull-right">
-                                            <li class="disabled"><a href="javascript:void(0)"><i class="fa fa-chevron-left"></i></a></li>
-                                            <li class="active"><a href="javascript:void(0)">1</a></li>
-                                            <li><a href="javascript:void(0)">2</a></li>
-                                            <li><a href="javascript:void(0)"><i class="fa fa-chevron-right"></i></a></li>
-                                        </ul>
-                                        <ul class="pagination remove-margin">
-                                            <li class="active"><span><strong>10</strong> Results</span></li>
+                                        
+                                        <ul class="pagination remove-margin ">
+                                            <li class="active"><span><strong><?php echo $business_info['total_rating']['total_no_of_reviews']; ?></strong> Results</span></li>
                                         </ul>
                                     </div>
                                     <!-- END Search Info - Pagination -->
@@ -95,32 +90,32 @@
                                     <table class="table table-striped table-vcenter">
                                         <thead>
                                             <tr>
-                                                <th><i class="fa fa-briefcase"></i> PROJECT</th>
-                                                <th class="text-center hidden-xs" style="width: 20%;">TICKETS</th>
-                                                <th class="text-center hidden-xs" style="width: 20%;">SALES</th>
-                                                <th class="text-center" style="min-width: 120px; width: 20%;">EARNINGS</th>
+                                                <th><i class="fa fa-briefcase"></i> Customer</th>
+                                                <th class="text-center hidden-xs" style="width: 20%;">rating</th>
+                                                <th class="text-center hidden-xs" style="width: 20%;">date</th>
+                                                <th class="text-center" style="min-width: 120px; width: 20%;">source</th>
                                             </tr>
                                         </thead>
-                                        <tbody>
-                                            <tr>
+                                        <tbody id="reviewBody">
+                                        <?php foreach($reviews as $eachreview){ ?>
+                                                <tr>
                                                 <td>
-                                                    <h3><a href="javascript:void(0)"><strong>Freelancer Web Application</strong></a></h3>
-                                                    <p>
-                                                        <a href="page_ready_user_profile.html" class="label label-default"><i class="fa fa-user"></i> Paul Porter</a>
-                                                        <a href="javascript:void(0)" class="label label-warning">PHP</a>
-                                                    </p>
-                                                    <p><em>Integer fermentum tincidunt auctor. Maecenas ultrices, justo vel imperdiet gravida, urna ligula hendrerit nibh, ac cursus nibh sapien in purus. Mauris tincidunt tincidunt turpis in porta. Integer fermentum tincidunt auctor. </em></p>
+                                                    <h3><a href="javascript:void(0)"><strong><?php echo $eachreview['customer_name']; ?></strong></a></h3>
+                                                    
+                                                    <p><em><?php echo $eachreview['description']; ?></em></p>
                                                 </td>
                                                 <td class="text-center hidden-xs">
-                                                    <h3 class="animation-pullDown">45</h3>
+                                                    <h3 class="animation-pullDown"><?php echo $eachreview['rating']; ?></h3>
                                                 </td>
                                                 <td class="text-center hidden-xs">
-                                                    <h3 class="animation-pullDown">3.265</h3>
+                                                    <h3 class="animation-pullDown"><?php echo $eachreview['date_of_submission']; ?></h3>
                                                 </td>
                                                 <td class="text-center">
-                                                    <h3 class="animation-pullDown text-success"><strong>$ 51.350</strong></h3>
+                                                    <h3 class="animation-pullDown text-success"><strong><?php echo $eachreview['review_source']; ?></strong></h3>
                                                 </td>
                                             </tr>
+                                        <?php } ?>
+                                            
                                             
                                         </tbody>
                                     </table>
@@ -128,11 +123,33 @@
 
                                     <!-- Bottom Navigation -->
                                     <div class="block-section text-right">
-                                        <ul class="pagination remove-margin">
-                                            <li class="disabled"><a href="javascript:void(0)"><i class="fa fa-chevron-left"></i></a></li>
-                                            <li class="active"><a href="javascript:void(0)">1</a></li>
-                                            <li><a href="javascript:void(0)">2</a></li>
-                                            <li><a href="javascript:void(0)"><i class="fa fa-chevron-right"></i></a></li>
+                                        <ul class="pagination remove-margin" id="bottomPagination">
+                                            <li class="disabled previouspage" >
+                                                <a href="javascript:void(0)" onclick="previousPage();"><i class="fa fa-chevron-left"></i></a>
+                                            </li>
+                                            <?php for($i=1;$i<=$pages;$i++){ 
+
+                                                if($i>3){ ?>
+                                                    <li class="hide pagingLink">
+                                                        <a href="javascript:void(0)" data-link="<?php echo $i; ?>" onclick="paginate(this);"><?php echo $i; ?></a>
+                                                    </li>    
+                                                <?php }elseif($i==1){
+                                                ?>
+                                                    <li class="active pagingLink">
+                                                        <a href="javascript:void(0)" data-link="<?php echo $i; ?>" onclick="paginate(this);"><?php echo $i; ?></a>
+                                                    </li>
+                                            <?php }else{
+                                                ?>
+                                                    <li class="pagingLink">
+                                                        <a data-link="<?php echo $i; ?>" href="javascript:void(0)" onclick="paginate(this);"><?php echo $i; ?></a>
+                                                    </li>
+                                            <?php }
+                                            } ?>
+                                            
+                                            
+                                            <li class="nextPage <?php echo ($pages<=2)?'disabled':''; ?>">
+                                                <a href="javascript:void(0)"  onclick="nextPage();"><i class="fa fa-chevron-right"></i></a>
+                                            </li>
                                         </ul>
                                     </div>
                                     <!-- END Bottom Navigation -->
@@ -154,10 +171,10 @@
                                     <!-- Info Title -->
                                     <div class="block-title">
                                         <div class="block-options pull-right">
-                                            <a href="javascript:void(0)" class="btn btn-alt btn-sm btn-default" data-toggle="tooltip" title="Friend Request"><i class="fa fa-plus"></i></a>
-                                            <a href="javascript:void(0)" class="btn btn-alt btn-sm btn-default" data-toggle="tooltip" title="Hire"><i class="fa fa-briefcase"></i></a>
+                                            
+                                            <a target="_blank" href="<?php echo $business_info['external_url']; ?>" class="btn btn-alt btn-sm btn-default" data-toggle="tooltip" title="Hire"><i class="fa fa-briefcase"></i></a>
                                         </div>
-                                        <h2>About <strong>John Doe</strong> <small>&bull; <i class="fa fa-file-text text-primary"></i> <a href="javascript:void(0)" data-toggle="tooltip" title="Download Bio in PDF">Bio</a></small></h2>
+                                        <h2>About <strong><?php echo $business_info['business_name']; ?></strong> <small>&bull; <i class="fa fa-file-text text-primary"></i> <a href="javascript:void(0)" data-toggle="tooltip" title="Download Bio in PDF">Bio</a></small></h2>
                                     </div>
                                     <!-- END Info Title -->
 
@@ -165,29 +182,22 @@
                                     <table class="table table-borderless table-striped">
                                         <tbody>
                                             <tr>
-                                                <td style="width: 20%;"><strong>Info</strong></td>
-                                                <td>Proin ac nibh rutrum lectus rhoncus eleifend. Sed porttitor pretium venenatis. Suspendisse potenti. Aliquam quis ligula elit. Aliquam at orci ac neque semper dictum. Sed tincidunt scelerisque ligula, et facilisis nulla hendrerit non.</td>
+                                                <td><strong>Name</strong></td>
+                                                <td><a href="javascript:void(0)"><?php echo $business_info['business_name']; ?></a></td>
                                             </tr>
                                             <tr>
-                                                <td><strong>Founder</strong></td>
-                                                <td><a href="javascript:void(0)">Company Inc</a></td>
+                                                <td style="width: 20%;"><strong>Address</strong></td>
+                                                <td><?php echo $business_info['business_address']; ?></td>
                                             </tr>
                                             <tr>
-                                                <td><strong>Education</strong></td>
-                                                <td><a href="javascript:void(0)">University Name</a></td>
+                                                <td><strong>Phone</strong></td>
+                                                <td><a href="javascript:void(0)"><?php echo $business_info['business_phone']; ?></a></td>
                                             </tr>
                                             <tr>
-                                                <td><strong>Projects</strong></td>
-                                                <td><a href="javascript:void(0)" class="label label-danger">168</a></td>
+                                                <td><strong>Rating</strong></td>
+                                                <td><a href="javascript:void(0)"><?php echo $business_info['total_rating']['total_avg_rating']; ?></a></td>
                                             </tr>
-                                            <tr>
-                                                <td><strong>Best Skills</strong></td>
-                                                <td>
-                                                    <a href="javascript:void(0)" class="label label-info">HTML</a>
-                                                    <a href="javascript:void(0)" class="label label-info">CSS</a>
-                                                    <a href="javascript:void(0)" class="label label-info">Javascript</a>
-                                                </td>
-                                            </tr>
+                                            
                                         </tbody>
                                     </table>
                                     <!-- END Info Content -->
@@ -222,20 +232,66 @@
         <!-- Bootstrap.js, Jquery plugins and Custom JS code -->
         <script src="<?php echo base_url();?>webroot/js/vendor/bootstrap.min.js"></script>
         
-        <script>
-        $(document).ready(function(){
-        	$.ajax({
-	                    type:"post",
-	                    url:"http://test.localfeedbackloop.com/api?apiKey=61067f81f8cf7e4a1f673cd230216112&noOfReviews=10&internal=0&yelp=1&google=1&offset=50&threshold=0",
-	                    data: "",
-	                    dataType:'JSONP',
-	                    success:function(data){
-	                    	obj = JSON.parse(data); 
-	                    	console.log(data);
-	                    }
-	                }); // Ajax Function Closing
+        <script type="text/javascript">
 
-        });
+        function previousPage(){
+            paginate($(".active.pagingLink").prev().find('a'));
+        }
+        function nextPage(){
+            paginate($(".active.pagingLink").next().find('a'));
+        }
+        function paginate(elem){
+            var pageNumber=$(elem).attr("data-link");
+            $(".pagingLink").removeClass("active");
+            $(elem).parent().addClass("active");
+            $(".pagingLink").addClass("hide");
+            $(elem).parent().removeClass("hide");
+            if($(elem).parent().prev().hasClass("pagingLink")){ 
+            //if there is a li behind this li
+                $(elem).parent().prev().removeClass("hide");
+            }else if($(elem).parent().next().next().hasClass("pagingLink")){ 
+            //if there is li next to next of this li
+                $(elem).parent().next().next().removeClass("hide");
+            }
+            
+
+            if($(elem).parent().next().hasClass("pagingLink")){
+                //if this there is li next to this li
+                $(elem).parent().next().removeClass("hide");
+            }else if($(elem).parent().prev().prev().hasClass("pagingLink")){
+                //if there is li at 2 position back of this li
+                $(elem).parent().prev().prev().removeClass("hide");
+            }
+            $(".nextPage").removeClass("disabled");
+            if($(".active.pagingLink").next().hasClass("nextPage")){
+                $(".nextPage").addClass("disabled");
+            }
+
+            $(".previouspage").removeClass("disabled");
+            if($(".active.pagingLink").prev().hasClass("previouspage")){
+                $(".previouspage").addClass("disabled");
+            }
+            
+            
+            $.ajax({
+                        type:"post",
+                        url:"<?php echo base_url();?>home/paginate",
+                        data: "pageNumber="+pageNumber,
+                        success:function(data){
+                            //console.log(data);
+                            $("#reviewBody").html('');
+                            var obj = JSON.parse(data); 
+                            $.each(obj, function(key,item) {
+                                var trdata='<tr><td><h3><a href="javascript:void(0)"><strong>'+item['customer_name']+'</strong></a></h3><p><em>'+item['description']+'</em></p></td><td class="text-center hidden-xs"><h3 class="animation-pullDown">'+item['rating']+'</h3></td><td class="text-center hidden-xs"><h3 class="animation-pullDown">'+item['date_of_submission']+'</h3></td><td class="text-center"><h3 class="animation-pullDown text-success"><strong>'+item['review_source']+'</strong></h3></td></tr>';
+                                $("#reviewBody").append(trdata);
+                                //console.log(value);
+                            });
+                            
+                        }
+                    }); // Ajax Function Closing
+
+        }
+       
         </script>
     </body>
 </html>
